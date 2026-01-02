b6361ec40e320321b199d71b4632c449ce75f091
shopifyapp470 <vamjamohit.vhits@gmail.com>
2026-01-02 16:05:00 +0530

    first commit

---
+++ prisma/migrations/20260102102323_init_sessions/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "shop" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "isOnline" BOOLEAN NOT NULL DEFAULT false,
+    "scope" TEXT,
+    "expires" TIMESTAMP(3),
+    "accessToken" TEXT NOT NULL,
+    "userId" BIGINT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT,
+    "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+    "locale" TEXT,
+    "collaborator" BOOLEAN DEFAULT false,
+    "emailVerified" BOOLEAN DEFAULT false,
+    "refreshToken" TEXT,
+    "refreshTokenExpires" TIMESTAMP(3),
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "userbirthday" (
+    "id" TEXT NOT NULL,
+    "shop" TEXT NOT NULL,
+    "birthdayPoint" INTEGER NOT NULL DEFAULT 0,
+    "anniversaryPoint" INTEGER NOT NULL DEFAULT 0,
+    "earnPercentage" INTEGER NOT NULL DEFAULT 10,
+    "minOrderTotal" DOUBLE PRECISION NOT NULL DEFAULT 10000.0,
+    "fixedRewardPoint" INTEGER NOT NULL DEFAULT 100,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "userbirthday_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "userbirthday_shop_key" ON "userbirthday"("shop");
